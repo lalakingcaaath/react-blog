92dda1dc8f45c4e3e74a3a2e5e324548f29c2a4d
Jericho <piojericho@gmail.com>
2026-02-02 23:06:52 +0800

    Added content to auth pages

--- src/auth/login.tsx
+++ src/auth/login.tsx
@@ -1,8 +1,49 @@
-export default function Login () {
-    return (
-        <div>
-            <h2>Login Page</h2>
-            <p>This is the login page.</p>
+import { useNavigate } from "react-router";
+
+export default function Login() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Login now!</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
         </div>
-    )
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <div className="card-body">
+            <fieldset className="fieldset">
+              <label className="label">Email</label>
+              <input
+                type="email"
+                className="input"
+                placeholder="Email"
+                required
+              />
+              <label className="label">Password</label>
+              <input
+                type="password"
+                className="input"
+                placeholder="Password"
+                required
+              />
+              <div>
+                <a
+                  className="link link-hover"
+                  onClick={() => navigate("/register")}
+                >
+                  Don't have an account yet?
+                </a>
+              </div>
+              <button className="btn btn-neutral mt-4">Login</button>
+            </fieldset>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/auth/register.tsx
+++ src/auth/register.tsx
@@ -1,8 +1,63 @@
-export default function Register () {
-    return (
-        <div>
-            <h2>Register Page</h2>
-            <p>This is the register page.</p>
+import { useNavigate } from "react-router";
+
+export default function Register() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Create your account now!</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
         </div>
-    )
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <div className="card-body">
+            <fieldset className="fieldset">
+              <label className="label">First Name</label>
+              <input
+                type="text"
+                className="input"
+                placeholder="First Name"
+                required
+              />
+              <label className="label">Last Name</label>
+              <input
+                type="text"
+                className="input"
+                placeholder="Last Name"
+                required
+              />
+              <label className="label">Email</label>
+              <input
+                type="email"
+                className="input"
+                placeholder="Email"
+                required
+              />
+              <label className="label">Password</label>
+              <input
+                type="password"
+                className="input"
+                placeholder="Password"
+                required
+              />
+              <div>
+                <a
+                  className="link link-hover"
+                  onClick={() => navigate("/login")}
+                >
+                  Already have an account?
+                </a>
+              </div>
+              <button className="btn btn-neutral mt-4">Login</button>
+            </fieldset>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }

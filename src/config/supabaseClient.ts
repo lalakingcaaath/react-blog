150060f7b52fbafbd68ef2c91cecc8f5e8882226
Jericho <piojericho@gmail.com>
2026-02-02 22:59:26 +0800

    Added connection to supabase

---
+++ src/config/supabaseClient.ts
@@ -0,0 +1,15 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+// Add a small check to help you debug
+if (!supabaseUrl || !supabaseKey) {
+  console.error(
+    "Supabase environment variables are missing! Check your .env file.",
+  );
+}
+
+export default supabase;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,23 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Bloglist from "../components/Bloglist";
+import supabase from "../config/supabaseClient";
 
 export default function Home() {
+  const testConnection = async () => {
+    const { data, error } = await supabase
+      .from("blog-post")
+      .select("*")
+      .limit(1);
+    if (error) {
+      console.error("Connection failed:", error.message);
+    } else {
+      console.log("Connection successful: ", data);
+    }
+
+    testConnection();
+  };
+
   return (
     <div>
       <Navbar />
